ad94f88732acb345beae06a1809bf850ee608b2e
Stephen Sarquah <stephen@stephensarquah.onmicrosoft.com>
2018-08-11 16:26:30 +0200

    Added bootstrap

--- app.js
+++ app.js
@@ -28,9 +28,12 @@ app.set('view engine', 'pug');
 app.use(express.json());
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+//Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Front end code
 app.get('/', (request, response) => {
